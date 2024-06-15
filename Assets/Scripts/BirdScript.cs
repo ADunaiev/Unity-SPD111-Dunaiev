b9ffacc1a0f326abde2b47311a21b4a89fed73ad
ADunaiev <111633434+ADunaiev@users.noreply.github.com>
2024-06-15 11:16:48 +0300

    Initial commit

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private Rigidbody2D rigidBody; // Посилання на інший компонент того ж ГО, що й скріпт
+
+    void Start()
+    {
+        Debug.Log("BirdScript Start");
+        // пошук компоненту та одержання посилання на нього
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(new Vector2(0, 500));
+        }
+    }
+}
